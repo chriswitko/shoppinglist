2abd3bb33caa75d540a6fc0cdfba420e8cd8e7d7
Chris Witko <chris@advantinteractive.com>
2015-02-26 08:07:28 +0100

    Updates to actions & new dispatcher, cleaning code

--- src/js/main.js
+++ src/js/main.js
@@ -7,6 +7,4 @@ var React = require('react');
 
 Parse.initialize('c3Ge9RO9ewCqsGyxvMnHTseQMFsn32qPG2gCXaB5', 'QkquMS88JO5O61Nu6DoPwnGOEECvtRKejuYwLxZ1');
 
-React.renderComponent(
-  <APP />,
-  document.getElementById('main'));
+React.renderComponent(<APP />, document.getElementById('main'));
